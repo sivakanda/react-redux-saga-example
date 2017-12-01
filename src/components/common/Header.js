ffd3ed5e35e411277142fc3fd67f4e66e99a2be6
sivakanda <33845064+sivakanda@users.noreply.github.com>
2017-12-01 10:59:06 -0500

    Add files via upload

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,1 +1,19 @@
+import React from 'react';
+import { Link, IndexLink } from 'react-router';
+//import '../../styles/style.css';
 
+const Header = () => (
+  <div className="text-center">
+    <nav className="navbar navbar-default">
+      <IndexLink to="/" activeClassName="active">Home</IndexLink>
+      {" | "}
+      <Link to="/accounts" activeClassName="active">Accounts</Link>
+      {" | "}
+      <Link to="/user-info" activeClassName="active">User Information</Link>
+      {" | "}
+      <Link to="/about" activeClassName="active">About</Link>
+    </nav>
+  </div>
+);
+
+export default Header;
