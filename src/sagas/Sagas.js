39d9df990efb5ce427f608c22148f7f2acc124eb
sivakanda <33845064+sivakanda@users.noreply.github.com>
2017-12-01 11:08:14 -0500

    Update Sagas.js

--- src/sagas/Sagas.js
+++ src/sagas/Sagas.js
@@ -1,1 +1,33 @@
+import {fork} from 'redux-saga/effects';
+import { put, call } from 'redux-saga/effects';
+import { fetchAccountList } from '../api/api';
+import * as types from '../constants/actionTypes';
+import { takeLatest } from 'redux-saga';
+
+//Worker Saga
+export function* fetchAccountsData({url, filter}) {
+    try {
+        const loading = true, done = false;
+        yield put({type: types.ITEMS_IS_LOADING, isLoading:loading});
+        const accountItems = yield call(fetchAccountList, url, filter);
+        yield [
+            put({type: types.ITEMS_IS_LOADING, isLoading:done}),
+            put({type: types.ITEMS_FETCH_DATA_SUCCESS, items:accountItems})
+        ];
+    }
+    catch (error) {
+        console.log("Error occured - " + error);
+        yield put({ type: types.ITEMS_HAS_ERRORED, hasErrored:true});
+    }
+}
+
+//Watcher Saga
+export function* watchFetchAccountData() {
+    yield* takeLatest(types.ITEMS_FETCH_DATA, fetchAccountsData);
+}
+
+//Root Saga
+export default function* rootSaga() {
+    yield fork(watchFetchAccountData);
+}
 
