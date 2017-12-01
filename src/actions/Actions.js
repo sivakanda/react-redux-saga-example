4e76f5910fd88fc3087600ee1164397a706e4c66
sivakanda <33845064+sivakanda@users.noreply.github.com>
2017-12-01 10:57:48 -0500

    Update Actions.js

--- src/actions/Actions.js
+++ src/actions/Actions.js
@@ -1,1 +1,27 @@
+import * as types from '../constants/actionTypes';
 
+export const itemsFetchData = (url, filter) => ({
+    type: types.ITEMS_FETCH_DATA,
+    url:url,
+    filter:filter
+});
+
+export const itemsHasErrored = (bool) => ({
+    type: types.ITEMS_HAS_ERRORED,
+    hasErrored: bool
+});
+
+export const itemsIsLoading = (bool) => ({
+    type: types.ITEMS_IS_LOADING,
+    isLoading: bool
+});
+
+export const itemsFetchDataSuccess = (items) => ({
+    type: types.ITEMS_FETCH_DATA_SUCCESS,
+    items
+});
+
+export const searchAccount = (accountName) => ({
+    type: types.ITEMS_SEARCH,
+    accountName
+});
