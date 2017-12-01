f6ae7eb7f98c6299d1a8d1357ddb803858f3ef47
sivakanda <33845064+sivakanda@users.noreply.github.com>
2017-12-01 10:58:14 -0500

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,1 +1,25 @@
 
+export const fetchAccountList = (url, filter) => {
+    debugger;
+    return fetch(url)
+    .then(response => {
+        debugger;
+        if (!response.ok) {
+            throw Error(response.statusText);
+        }
+        return response.json();
+    }).then(json => {
+        debugger;
+        let filteredObject = null;
+        Object.keys(json).forEach(function(k){
+            if(k===filter) {
+                filteredObject = json[k];
+            }
+        });
+        return filteredObject;
+    }).catch(function(error) {
+        debugger;
+        console.log('There has been a problem with your fetch operation: ' + error.message);
+    });
+};
+
