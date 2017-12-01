7286968658e0732c8114f38f3a7a27ab27b266fe
sivakanda <33845064+sivakanda@users.noreply.github.com>
2017-12-01 11:07:10 -0500

    Add files via upload

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,1 +1,17 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+import App from '../containers/App';
+import HomePage from '../components/HomePage';
+import ItemList from '../components/ItemList';
+import UserDetailsContainer from'../containers/UserDetailsContainer';
+import AccountDetailsContainer from '../containers/AccountDetailsContainer';
+
+export default (
+  <Route path="/" component={App}>
+    <IndexRoute component={HomePage} />
+    <Route path="accounts" component={AccountDetailsContainer} />
+    <Route path="user-info" component={UserDetailsContainer} />
+    <Route path="about" component={ItemList} />
+  </Route>
+);
 
