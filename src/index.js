8722ceb05fb23cc60552d44cd1f7932ccd236db1
sivakanda <33845064+sivakanda@users.noreply.github.com>
2017-12-01 10:30:35 -0500

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 
+import { render } from 'react-dom';
+import configureStore from './store/configureStore';
+import React from 'react';
+import { Router, createHashHistory } from 'react-router';
+import { Provider } from 'react-redux';
+import routes from './routes/routes';
+
+const store = configureStore();
+
+render(
+  <Provider store={store}>
+    <Router history={createHashHistory} routes={routes} />
+  </Provider>, document.getElementById('app')
+);
